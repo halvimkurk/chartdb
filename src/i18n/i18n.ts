88e0c3a69825a2b48e06066dff4773a02121646b
Anton Ishanov <73119334+halvimkurk@users.noreply.github.com>
2024-11-06 19:39:20 +0300

    Update i18n.ts

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -9,6 +9,7 @@ import { hi, hiMetadata } from './locales/hi';
 import { ja, jaMetadata } from './locales/ja';
 import { pt_BR, pt_BRMetadata } from './locales/pt_BR';
 import { uk, ukMetadata } from './locales/uk';
+import { ru, ruMetadata } from './locales/ru';
 
 export const languages: LanguageMetadata[] = [
     enMetadata,
@@ -19,6 +20,7 @@ export const languages: LanguageMetadata[] = [
     jaMetadata,
     pt_BRMetadata,
     ukMetadata,
+    ruMetadata,
 ];
 
 const resources = {
@@ -30,6 +32,7 @@ const resources = {
     ja,
     pt_BR,
     uk,
+    ru,
 };
 
 i18n.use(initReactI18next).init({
